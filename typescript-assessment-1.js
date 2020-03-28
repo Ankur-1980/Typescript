c911145a08de7a632cc4becfe3ea0652b2a6a37f
Ankur <vasavada.ankur@gmail.com>
2020-03-28 13:15:42 -0400

    Done

--- typescript-assessment-1.js
+++ typescript-assessment-1.js
@@ -1,49 +1,71 @@
 "use strict";
-// interface Mountain {
-// 	name: string;
-// 	height: number;
-// }
-// const mountains: Mountain[] = [
-// 	{
-// 		name: 'Kilamanjaro',
-// 		height: 19341,
-// 	},
-// 	{
-// 		name: 'Everest',
-// 		height: 29029,
-// 	},
-// 	{
-// 		name: 'Denali',
-// 		height: 20310,
-// 	},
-// ];
-// function findNameOfTallestMountain(mtns: Mountain[]) {
-// 	const tallest = mtns.reduce(function(acc, curr) {
-// 		return acc.height > curr.height ? acc : curr;
-// 	});
-// 	console.log(tallest.name);
-// }
-// findNameOfTallestMountain(mountains);
-// interface Product {
-// 	name: string;
-// 	price: number;
-// }
-// const products: Product[] = [
-// 	{
-// 		name: 'Meletti',
-// 		price: 10,
-// 	},
-// 	{
-// 		name: 'Nonino',
-// 		price: 15,
-// 	},
-// 	{
-// 		name: 'Montenegro',
-// 		price: 20,
-// 	},
-// ];
-// function calcAverageProductPrice(prods: Product[]) {
-// 	const average = prods.reduce((acc, curr) => acc + curr.price, 0) / prods.length;
-// 	console.log(average);
-// }
-// calcAverageProductPrice(products);
+const mountains = [
+    {
+        name: 'Kilamanjaro',
+        height: 19341,
+    },
+    {
+        name: 'Everest',
+        height: 29029,
+    },
+    {
+        name: 'Denali',
+        height: 20310,
+    },
+];
+function findNameOfTallestMountain(mtns) {
+    // use reduce because we need only one value
+    const tallest = mtns.reduce((acc, curr) => 
+    // if the starting height is greater than the current height return accordingly
+    acc.height > curr.height ? acc : curr);
+    console.log(tallest.name);
+}
+findNameOfTallestMountain(mountains);
+const products = [
+    {
+        name: 'Meletti',
+        price: 10,
+    },
+    {
+        name: 'Nonino',
+        price: 15,
+    },
+    {
+        name: 'Montenegro',
+        price: 20,
+    },
+];
+function calcAverageProductPrice(prods) {
+    // again reduce because we need one value
+    //acc is NaN to start with, so it doesn't have a .price.
+    const average = prods.reduce((acc, curr) => acc + curr.price, 0) / prods.length;
+    console.log(average);
+}
+calcAverageProductPrice(products);
+const inventory = [
+    {
+        name: 'motor',
+        price: 10.0,
+        quantity: 10,
+    },
+    {
+        name: 'sensor',
+        price: 12.5,
+        quantity: 4,
+    },
+    {
+        name: 'LED',
+        price: 1,
+        quantity: 20,
+    },
+];
+function calcInventoryValue(stock) {
+    // first get the totals of the individual items
+    const subTotals = stock.map(items => items.price * items.quantity);
+    // returns an array of the totals [100,50,20]
+    console.log(subTotals);
+    // reduce to add everything in the subtotals array together.
+    const total = subTotals.reduce((acc, curr) => (acc += curr));
+    console.log(total);
+}
+calcInventoryValue(inventory);
