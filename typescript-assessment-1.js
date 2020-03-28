bbcba3af185948375d3c80ff71d23cd86852e619
Ankur <vasavada.ankur@gmail.com>
2020-03-28 11:34:55 -0400

    Finished with 2/3 of the assignment

---
+++ typescript-assessment-1.js
@@ -0,0 +1,49 @@
+"use strict";
+// interface Mountain {
+// 	name: string;
+// 	height: number;
+// }
+// const mountains: Mountain[] = [
+// 	{
+// 		name: 'Kilamanjaro',
+// 		height: 19341,
+// 	},
+// 	{
+// 		name: 'Everest',
+// 		height: 29029,
+// 	},
+// 	{
+// 		name: 'Denali',
+// 		height: 20310,
+// 	},
+// ];
+// function findNameOfTallestMountain(mtns: Mountain[]) {
+// 	const tallest = mtns.reduce(function(acc, curr) {
+// 		return acc.height > curr.height ? acc : curr;
+// 	});
+// 	console.log(tallest.name);
+// }
+// findNameOfTallestMountain(mountains);
+// interface Product {
+// 	name: string;
+// 	price: number;
+// }
+// const products: Product[] = [
+// 	{
+// 		name: 'Meletti',
+// 		price: 10,
+// 	},
+// 	{
+// 		name: 'Nonino',
+// 		price: 15,
+// 	},
+// 	{
+// 		name: 'Montenegro',
+// 		price: 20,
+// 	},
+// ];
+// function calcAverageProductPrice(prods: Product[]) {
+// 	const average = prods.reduce((acc, curr) => acc + curr.price, 0) / prods.length;
+// 	console.log(average);
+// }
+// calcAverageProductPrice(products);
